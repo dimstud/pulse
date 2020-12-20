e3e1929708e965e85bac45e23e614ed2a2297df4
Dmitry Ageev <dimichageev@gmail.com>
2020-12-20 14:51:27 +0300

    add mask phone

--- src/js/script.js
+++ src/js/script.js
@@ -84,4 +84,6 @@ $(document).ready(function () {
     validateForms('#consultation-form');
     validateForms('#order form');
     validateForms('#consultation form');
+
+    $('input[name=phone]').mask("+7 (999) 999-9999");
 });
